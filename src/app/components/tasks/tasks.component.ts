8bf918062b9a6d2d2a5d376b585fae1c4c80b7c5
Felixx-jnr <uyuoukoh@gmail.com>
2024-08-22 14:17:10 +0100

    task setup

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
+import { TaskItemComponent } from '../task-item/task-item.component';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [CommonModule, TaskItemComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+}
