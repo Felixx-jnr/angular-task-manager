9df137b995612bbee49598f8e916e4eb2c46c937
Felixx-jnr <uyuoukoh@gmail.com>
2024-08-28 14:12:58 +0100

    reminder toggle setup

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -14,9 +14,15 @@ import { CommonModule } from '@angular/common';
 export class TaskItemComponent {
   @Input() task: Task = {} as Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
