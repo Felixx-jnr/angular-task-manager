ec428a0607f64d1a9b91f4278c18b53c83c1937d
Felixx-jnr <uyuoukoh@gmail.com>
2024-08-28 12:32:37 +0100

    delete function

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,13 +3,15 @@ import { Task } from '../../Task';
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../../services/task.service';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, HttpClientModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
+  providers: [TaskService],
 })
 export class TasksComponent {
   tasks: Task[] = [];
@@ -17,6 +19,14 @@ export class TasksComponent {
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
   }
 }
